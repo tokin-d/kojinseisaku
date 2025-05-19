7ceecb73319df498587de2a95419ab60e558db0a
DESKTOP-IFSN5VC\1220AM <tokinemt@gmail.com>
2025-05-19 13:34:06 +0900

    0519

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class P : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigid2D;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 1.0f, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -1.0f, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1.0f, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(1.0f, 0, 0);
+        }
     }
 }
